a5d2ad69e15873ae183f6af2df117ded9e780eb4
Coto <programmer.coto.1988@gmail.com>
2017-12-12 02:22:50 -0400

    update IPC Version: 1.3

--- common/specific_shared.cpp
+++ common/specific_shared.cpp
@@ -18,6 +18,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
 USA
 
 */
+//TGDS required version: IPC Version: 1.3
 
 #include "ipcfifo.h"
 #include "specific_shared.h"
@@ -42,6 +43,11 @@ USA
 
 #endif
 
+//Coto: Hardware IPC struct packed 
+struct sIPCSharedTGDSSpecific* getsIPCSharedTGDSSpecific(){
+	struct sIPCSharedTGDSSpecific* sIPCSharedTGDSSpecificInst = (__attribute__((packed)) struct sIPCSharedTGDSSpecific*)(getUserIPCAddress());
+	return sIPCSharedTGDSSpecificInst;
+}
 
 //inherits what is defined in: common_shared.c
 #ifdef ARM9
