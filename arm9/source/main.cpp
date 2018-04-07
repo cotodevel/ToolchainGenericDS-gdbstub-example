59779dcf6f133f87d785176ee49887d1f6da1b62
cotodevel <programmer.coto.1988@gmail.com>
2018-04-07 02:22:57 -0300

    update GDB code

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -99,9 +99,12 @@ int main(int _argc, sint8 **_argv) {
 		}
 		
 		//GDB Stub Process must run here
-		if(remoteStubMain() == remoteStubMainWIFINotConnected){
+		int retGDBVal = remoteStubMain();
+		if(retGDBVal == remoteStubMainWIFINotConnected){
 			if (switch_dswnifi_mode(dswifi_gdbstubmode) == true){
+				clrscr();
 				//Show IP and port here
+				printf("[Connect to GDB]:");
 				printf("Port:%d GDB IP:%s",remotePort,(char*)print_ip((uint32)Wifi_GetIP()));
 				remoteInit();
 			}
@@ -109,7 +112,17 @@ int main(int _argc, sint8 **_argv) {
 				//GDB Client Reconnect:ERROR
 			}
 		}
-		//else should be connected and GDB running at desired IP/port
+		else if(retGDBVal == remoteStubMainWIFIConnectedGDBDisconnected){
+			setWIFISetup(false);
+			if (switch_dswnifi_mode(dswifi_gdbstubmode) == true){ // gdbNdsStart() called
+				reconnectCount++;
+				clrscr();
+				//Show IP and port here
+				printf("[Re-Connect to GDB]:So far:%d time(s)",reconnectCount);
+				printf("Port:%d GDB IP:%s",remotePort,(char*)print_ip((uint32)Wifi_GetIP()));
+				remoteInit();
+			}
+		}
 		
 		IRQVBlankWait();
 	}
